b61c8c8d7d9dbe5659561fcec8a60c53866e161e
Ardian <ardian11@ui.ac.id>
2023-02-26 22:43:49 +0700

    Models

--- Tugas_1/study_tracker/models.py
+++ Tugas_1/study_tracker/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class TransactionRecord(models.Model):
+    name = models.CharField(max_length=50)
+    subject = models.CharField(max_length=50)
+    date = models.DateTimeField(auto_now_add=True)
+    progress = models.IntegerField()
+    description = models.TextField()
