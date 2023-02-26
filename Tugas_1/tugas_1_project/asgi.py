cb2896fa12caf164476ce89605b9895e5c1bfd52
Ardian <ardian11@ui.ac.id>
2023-02-26 22:39:30 +0700

    Ubah nama project & start app money_tracker

--- Tugas_1/tugas_1_project/asgi.py
+++ Tugas_1/tugas_1_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for study_tracker project.
+ASGI config for tugas_1_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'study_tracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tugas_1_project.settings')
 
 application = get_asgi_application()
--- Tugas_1/tugas_1_project/wsgi.py
+++ Tugas_1/tugas_1_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for study_tracker project.
+WSGI config for tugas_1_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'study_tracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tugas_1_project.settings')
 
 application = get_wsgi_application()
